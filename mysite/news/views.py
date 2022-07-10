e39c90cec6e0fe642bacdb69d8ed0ea3fcb58af0
krbtsv <maxim.karabtsov@gmail.com>
2022-07-10 17:21:26 +0300

    Create class CreateNews

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import News, Category
 from .forms import NewsForm
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 class HomeNews(ListView):
     model = News
@@ -25,6 +25,12 @@ class ViewNews(DetailView):
     context_object_name = 'news_item'
     # pk_url_kwarg = 'news_id'
     # template_name = 'news/news_detail.html'
+
+
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')
 
 
 # def index(request):
@@ -59,20 +65,20 @@ def get_category(request, category_id):
     return render(request, 'news/category.html', {'news': news, 'category': category})
 
 
-def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {'news_item': news_item})
+# def view_news(request, news_id):
+#     news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, 'news/view_news.html', {'news_item': news_item})
 
 
 
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            # news = News.objects.create(**form.cleaned_data)
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             # news = News.objects.create(**form.cleaned_data)
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})
