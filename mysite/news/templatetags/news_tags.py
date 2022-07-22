5119e35d6decdf057ed9c0456358c20408e646cd
krbtsv <maxim.karabtsov@gmail.com>
2022-07-22 19:13:16 +0300

    Adding filter for current number of news

--- mysite/news/templatetags/news_tags.py
+++ mysite/news/templatetags/news_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from django.db.models import Count
+from django.db.models import Count, F
 from news.models import Category
 
 register = template.Library()
@@ -12,5 +12,6 @@ def get_categories(): #Теперь данный тег получает спи�
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hellow', arg2='World'): #Данный тег получает данные, рендерит шаблон и возвращает шаблон с данными
     # categories = Category.objects.all()
-    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    # categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
     return {'categories': categories, 'arg1': arg1, 'arg2': arg2}
