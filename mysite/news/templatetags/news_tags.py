3eb69f31b00c41b0c9b04df3ac874619e8306e09
krbtsv <maxim.karabtsov@gmail.com>
2022-07-28 18:57:15 +0300

    Testing cache

--- mysite/news/templatetags/news_tags.py
+++ mysite/news/templatetags/news_tags.py
@@ -1,5 +1,8 @@
 from django import template
 from django.db.models import Count, F
+from django.core.cache import cache
+
+
 from news.models import Category
 
 register = template.Library()
@@ -11,7 +14,9 @@ def get_categories(): #Теперь данный тег получает спи�
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hellow', arg2='World'): #Данный тег получает данные, рендерит шаблон и возвращает шаблон с данными
-    # categories = Category.objects.all()
-    # categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    # categories = cache.get('categories')
+    # if not categories:
+    #     categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
+    #     cache.set('categories', categories, 30)
     categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
     return {'categories': categories, 'arg1': arg1, 'arg2': arg2}
